2fa0fdb66950ed8cae8f47735cbbcf38233888ca
Niko <n.dunkel@gmail.com>
2020-10-13 16:47:47 -0700

    rebuilding site Tue Oct 13 16:47:46 PDT 2020

--- api/coupon.js
+++ api/coupon.js
@@ -2,8 +2,15 @@
 
 module.exports = async (req, res) => {
   try {
-    const { name } = req.query
-  	res.status(200).send(`Hello ${name}!`)
+    const { code } = req.query
+    let response = false
+    if(code === 'joe20'){
+      response = true
+    }
+    if(code === 'latchcollective20'){
+      response = true
+    }
+    res.status(200).send(response)
   } catch(error){
     console.log(error);
     res.end()
