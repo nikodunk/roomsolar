b411894c54e5696741fa605a6ee472e5d1e1f12f
Niko <n.dunkel@gmail.com>
2020-10-26 17:01:54 -0700

    rebuilding site Mon Oct 26 17:01:54 PDT 2020

--- api/coupon.js
+++ api/coupon.js
@@ -4,7 +4,12 @@ module.exports = async (req, res) => {
   try {
     let { code } = req.query
     code = code.toLowerCase()
-    const validCodes = [ 'resource', 'latch', 'maxedoutsolar', 'katiepatrick']
+    const validCodes = [
+      'resource',
+      'latch', 
+      'maxedoutsolar', 
+      'katiepatrick'
+    ]
     let response = validCodes.includes(code); // returns true or false
     res.status(200).send(response)
   } catch(error){
