25da50969e346ada81a7e261a68c11795be2c283
Niko <n.dunkel@gmail.com>
2021-03-24 20:09:48 -0700

    newest blog source deploy Wed Mar 24 20:09:48 PDT 2021

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-# Build the project.
-# hugo
+git add .
+git commit -m "newest blog source deploy `date`"
+
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-git add .
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
 git push origin main
